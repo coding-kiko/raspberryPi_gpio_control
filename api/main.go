844a1319ae45972a9941fb1df7f59d23da8625e7
coding-kiko <kikoaudi2001@gmail.com>
2022-07-09 15:53:36 -0300

    initial

---
+++ api/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	// std lib
+	"log"
+	"net/http"
+
+	// third party
+	"github.com/stianeikeland/go-rpio"
+)
+
+const pinNumber = 1
+
+func main() {
+
+	err := rpio.Open()
+	if err != nil {
+		log.Fatalln("Unable to open gpio connection")
+	}
+	defer rpio.Close()
+
+	pin := rpio.Pin(pinNumber)
+
+	mux := http.NewServeMux()
+	mux.Handle("/toggle", http.HandlerFunc(func(_ http.ResponseWriter, _ *http.Request) {
+		log.Println("toggled")
+		pin.Toggle()
+	}))
+	log.Fatalln(http.ListenAndServe("0.0.0.0:8000", mux))
+}
+
+/* func TogglePin(pin rpio.Pin) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		pin.Toggle()
+	})
+} */
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/coding-kiko/gpio_control
+
+go 1.13
+
+require github.com/stianeikeland/go-rpio v4.2.0+incompatible
